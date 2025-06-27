abf8951947ae72a675522bdcd40f998a6a2c3d57
j-fal <joao.antunes@gridtech.com.br>
2025-06-27 11:49:54 -0300

    FIX: Adjustment to migrations due to deprecated line of code. Adjustment to schema. Adjustment to Dockerfile to fixate the gem bundler version. Adjustment to docker entrypoints to add main folder as safe directory.

--- docker/entrypoints/vite.sh
+++ docker/entrypoints/vite.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -x
 
+git config --global --add safe.directory /app
+
 rm -rf /app/tmp/pids/server.pid
 rm -rf /app/tmp/cache/*
 
 pnpm store prune
-pnpm install --force
+pnpm install --force --reporter=append-only --loglevel=debug
 
 echo "Ready to run Vite development server."
 
